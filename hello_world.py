3b9aeb4dd0fe5a40ea60294b9d5f80a9a9fc62ee
gaffey.wei <gaffey.wei@wecosmos.com>
2023-11-22 13:27:39 +0800

    Initial commit

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,14 @@
-print('hello world')
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api', methods=['GET', 'POST'])
+def my_api():
+    if request.method == 'POST':
+        data = request.json
+        print('hello world')
+        return jsonify({"message": "Data received", "data": data})
+    return jsonify({"message": "Hello, World!"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
